3d855821c06376a918ae289ccacd5a8d601f2064
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2022-11-16 11:18:52 +0330

    create router

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../Pages/Home/Home";
+import SinglePage from "../Pages/SinglePage/SinglePage";
 
 const router =createBrowserRouter([
-  
+    {
+        path: '/',
+        element: <Home/>
+    },
+    {
+        path: 'weather/:city',
+        element: <SinglePage/>
+    }
 ])
 export default router
