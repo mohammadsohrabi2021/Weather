6f6a9ec22663d7fb6a921470027fd0f712dab615
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2022-11-16 13:11:38 +0330

    change header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,16 @@
-import { Grid, Typography } from '@mui/material';
+import { Container, Grid, Typography } from '@mui/material';
 import React from 'react';
-
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
 const Header = () => {
     return (
-        <Grid item xs={12} textAlign={'center'}>
-            <Typography variant={'h5'}>
-                Weather App
-            </Typography>
-        </Grid>
+        <Container>
+            <Grid item xs={12} display={'flex'} justifyContent={'space-between'} alignItems={'center'} color={'#fff'} >
+                <Typography variant={'h5'}>
+                    {'وضعیت آب و هوا'}
+                </Typography>
+                <ThunderstormIcon />
+            </Grid>
+        </Container>
     );
 };
 
