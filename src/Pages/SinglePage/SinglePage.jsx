64d91f7161278ec9426a52039c106e5b3d6f3d76
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2022-11-16 11:16:53 +0330

    create single page

--- src/Pages/SinglePage/SinglePage.jsx
+++ src/Pages/SinglePage/SinglePage.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import { Grid } from '@mui/material';
+import React,{useEffect, useState} from 'react';
+import {useParams} from "react-router-dom";
+import handleGetFiveDayWeather from '../../Api/Api'
+import WeatherFutureCard from '../../Components/WeatherFutureCard/WeatherFutureCard'
+
+
+const daysIndex = [0, 8, 16, 24, 32]
+
 
 const SinglePage = () => {
+    const [cityWeatherFuture, setCityWeatherFuture] = useState({})
+    const { city } = useParams()
+
+    const handleGetData = async () => {
+        const data = await handleGetFiveDayWeather(city)
+        setCityWeatherFuture(data)
+    }
+
+    useEffect(() => {
+        handleGetData()
+    }, [])
+
     return (
-        <div>
-            
-        </div>
-    );
+        <Grid>
+            <Grid>
+                {city}
+            </Grid>
+            <Grid>
+                {daysIndex.map(index => <WeatherFutureCard key={index} index={index}
+                    cityWeatherFuture={cityWeatherFuture} />)}
+            </Grid>
+        </Grid>
+    )
 };
 
 export default SinglePage;
