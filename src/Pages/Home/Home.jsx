6879065dee355219580474bb5731a3c9d23b0c2e
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2022-11-16 11:13:33 +0330

    create home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import { Grid } from '@mui/material';
+import React,{useEffect, useState} from 'react';
+import handleGetOneDayWeather from '../../Api/Api'
+import Header from '../../Components/Header/Header'
+import SelectCity from '../../Components/SelectCity/SelectCity'
+import WeatherCard from '../../Components/WeatherCard/WeatherCard'
 
 const Home = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    const [city, setCity] = useState('tehran');
+    const [cityWeather, setCityWeather] = useState({})
+
+    const handleGetData = async () => {
+        const data = await handleGetOneDayWeather(city)
+        setCityWeather(data)
+    }
+
+    useEffect(() => {
+        handleGetData()
+    }, [city])
+
+    return(
+        <Grid container>
+            <Header/>
+            <SelectCity currency={city} setCurrency={setCity}/>
+            <WeatherCard cityWeather={cityWeather} city={city}/>
+        </Grid>
+    )
 };
 
 export default Home;
